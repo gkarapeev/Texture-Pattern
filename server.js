132971190d55063ab6ae82879cd32fe447407f04
asipetrov <asitopetrov@gmail.com>
2020-04-22 22:11:16 +0300

    delete shit

--- server.js
+++ server.js
@@ -3,18 +3,6 @@ const fs = require('fs');
 const port = process.env.PORT || 8080;
 
 const serverFunction = function (request, response) {
-  if (request.url === '/') {
-    fs.readFile('front-end/index.html', (error, data) => {
-      if (error) {
-        response.writeHead(404);
-        response.write('Error: File not found.');
-      }
-
-      response.writeHead(200, { 'Content-type': 'text/html' });
-      response.write(data);
-      response.end();
-    });
-  }
 
   if (request.url === '/styles.css') {
     fs.readFile('front-end/styles.css', function (err, data) {
